7ad878c61d42229450b2cb2c3e988bbcf5f86a2a
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-08-27 15:48:33 +0200

    doing some search bars in booklist

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,15 +1,30 @@
 import SingleBook from "./SingleBook";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import { useState } from "react";
 
 const BookList = (props) => {
+  const [bar, setBar] = useState("");
+
   return (
     <Container>
+      <Row className="justify-content-center my-3">
+        <Col xs={12} md={6}>
+          <Form.Control
+            type="text"
+            placeholder="Search for a book..."
+            value={bar}
+            onChange={(e) => setBar(e.target.value)}
+          />
+        </Col>
+      </Row>
       <Row>
-        {props.books.map((libro) => (
-          <Col xs={12} md={4}>
-            <SingleBook img={libro.img} title={libro.title} />
-          </Col>
-        ))}
+        {props.books
+          .filter((b) => b.title.toLowerCase().includes(bar.toLowerCase()))
+          .map((libro) => (
+            <Col xs={12} md={4} key={libro.asin}>
+              <SingleBook img={libro.img} title={libro.title} />
+            </Col>
+          ))}
       </Row>
     </Container>
   );
